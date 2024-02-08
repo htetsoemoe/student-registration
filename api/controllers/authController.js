710c5793a8e292335ef8857c9c64accf2fe8656c
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-02-08 17:08:10 +0630

    finished signin and signout api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -37,6 +37,56 @@ const signUp = async (req, res, next) => {
     }
 }
 
+// @desc Sign In middleware
+// @route POST /signin
+// @access Public
+const signIn = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return next(errorHandler(400, "All Fields are required"))
+    }
+
+    try {
+        const validUser = await Teacher.findOne({ email })
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found : Wrong Email'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return next(errorHandler(401, 'Wrong Password'))
+        }
+
+        // Generate JWT Token
+        const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET)
+
+        // Remove validUser's password
+        const { password: pass, ...rest } = validUser._doc
+
+        res.cookie('token', token)
+            .status(200)
+            .json(rest)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+// @desc Signout Out middleware
+// @route GET /signout
+// @access Public
+const signOut = async (req, res, next) => {
+    try {
+        res.cookie('token', '')
+        res.status(200).json('User has been logout')
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     signUp,
+    signIn,
+    signOut
 }
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -5,4 +5,10 @@ const authController = require('../controllers/authController')
 router.route('/signup')
     .post(authController.signUp)
 
+router.route('/signin')
+    .post(authController.signIn)
+
+router.route('/signout')
+    .get(authController.signOut)
+
 module.exports = router
